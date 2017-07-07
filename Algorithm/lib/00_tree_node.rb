8d48a320522d8c6bdb310aa26da7840e2ff528ea
Omayemi Agbeyegbe <grend500@gmail.com>
2017-07-07 12:28:01 -0400

    Finish polytree and change folder name

---
+++ Algorithm/lib/00_tree_node.rb
@@ -0,0 +1,49 @@
+class PolyTreeNode
+  attr_accessor :parent, :children
+  attr_reader :value
+
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def parent=(node)
+    unless node == @parent
+      @parent.children.delete(self) unless @parent == nil
+      @parent = node
+      @parent.children << self unless node == nil
+    end
+  end
+
+  def add_child(child_node)
+    child_node.parent = self
+  end
+
+  def remove_child(child_node)
+    raise("not a child") unless children.include?(child_node)
+    child_node.parent = nil
+  end
+
+  def dfs(target_val)
+    return self if self.value == target_val
+    # debugger
+    self.children.each do |child|
+      child_result = child.dfs(target_val)
+      return child_result if child_result
+    end
+
+    nil
+  end
+
+  def bfs(target_val)
+    queue = [self]
+    until queue.empty?
+      node = queue.shift
+      return node if node.value == target_val
+      queue += node.children
+    end
+
+  end
+
+end
